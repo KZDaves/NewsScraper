d40e577d4110a27f6ac4cbced998a77bffc07192
KZD <45324536+KZDaves@users.noreply.github.com>
2019-04-30 22:08:41 -0700

    scraped stories now posting to page

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var axios = require("axios");
 var cheerio = require("cheerio"); 
 
 var app = express();
+app.set('view engine', 'ejs'); 
 var databaseUrl = "scraper"; 
 var collections = ["scrapedData"]; 
 
@@ -12,23 +13,33 @@ db.on("error", function(error){
 	console.log("Error: ", error); 
 }); 
 
+
+app.get('/', function(req, res){
+	res.render('pages/index', {
+		stories: []
+	}); 
+})
+
 app.get("/all", function(req, res){
 	db.scrapedData.find({}, function(error, found){
 		if(error) throw error; 
-		else res.json(found); 
+		else res.render('pages/index', {
+			stories: found
+		})
 	})
 })
 
-app.get("/scrape", function(req, res){
+app.post("/scrape", function(req, res){
 	axios.get("https://www.newyorker.com/magazine/fiction").then(function(response){
 		var $ = cheerio.load(response.data); 
 		$(".River__hed___re6RP").each(function(i, element){
-			var title = $(element).text();
+			var t = $(element).text();
 			var summary = $(element).parent().siblings(".River__riverItemBody___347sz").children("h5").text();
 			var link = 'https:www.newyorker.com' + $(element).parent().attr("href"); 
-			if (title){
+
+			if (t && summary && link){
 				db.scrapedData.insert({
-					title: title, 
+					title: t, 
 					summary: summary,
 					link: link
 				},
@@ -36,10 +47,11 @@ app.get("/scrape", function(req, res){
 					if(err) throw err; 
 					else console.log(inserted); 
 				});
+				//console.log('made it');
 			}
 		});
 	});
-	res.send("scrape complete"); 
+	res.redirect('/all'); 
 });
 
 
